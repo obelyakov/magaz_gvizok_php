6544c6929d15ff03f64b785ac929414ecca604b8
unknown <C:\Users\\AppData\Roaming\The Bat!>
2012-09-16 02:13:17 +0400

    Хаки для загрузки файлов зип и пдф

--- system/application/controllers/admin/goods.php
+++ system/application/controllers/admin/goods.php
@@ -252,7 +252,7 @@ class Goods extends Controller {
     function upload_file()
     {
         $config['upload_path'] = './uploads/';
-        $config['allowed_types'] = 'pdf|zip|rar|xls|doc';
+        $config['allowed_types'] = 'application/octet-stream|application/pdf|zip|pdf';
         $config['max_size']	= '2048';
         #$config['max_width']  = '1024';
         #$config['max_height']  = '768';
